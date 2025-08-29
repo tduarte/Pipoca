56daf0c4bd2acd6a7f12ed66a310a09cbfcf6d5b
Thiago Duarte <me@tduarte.co>
2025-08-28 19:44:00 -0700

    I knew it

--- scripts/esbuild.js
+++ scripts/esbuild.js
@@ -1,15 +1,22 @@
 import {argv} from "node:process";
+import {fileURLToPath} from "node:url";
+import path from "node:path";
 import {build} from "esbuild";
 
 const [, , entryPoint, outdir] = argv;
 
 if (entryPoint && outdir) {
+  const thisDir = path.dirname(fileURLToPath(import.meta.url));
+  const repoRoot = path.resolve(thisDir, "..");
+  const tsconfig = path.join(repoRoot, "tsconfig.esbuild.json");
+
   await build({
     bundle: true,
     entryPoints: [entryPoint],
     external: ["gettext", "gi://*"],
     format: "esm",
     loader: {".ui": "text"},
+    tsconfig,
     outdir: outdir,
     sourcemap: true,
     target: "es2022",
